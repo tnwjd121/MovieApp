9f459c97cc68a4b09e18065da995a11bb5164e1c
tnwjd121 <aojj121@gmail.com>
2024-04-11 14:47:24 +0900

    PPT작성

--- src/main/MovieApp.java
+++ src/main/MovieApp.java
@@ -5,7 +5,11 @@ public class MovieApp {
 	
 	public static void main(String[] args) {
 		MovieController movieController = new MovieController();
+		
+		// 앱 시작
 		movieController.runStartApp();
+		
+		
 	}
 	
 
